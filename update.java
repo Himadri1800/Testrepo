d1884fd002c36cf64864e2252be9e449abdf35c7
Himadri1800 <himadri1800@gmail.com>
2013-02-12 05:39:23 -0800

    My First commit!

---
+++ update.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
+
+class updatejar
+{
+	public static void main(String[] args)
+	{
+		File[] contents = {new File("AbstractNodeCreator.class"),
+		new File("C:\\Documents and Settings\\Himadri Mandal\\Desktop\\him\\sample2.txt")
+		};
+	}
+};
